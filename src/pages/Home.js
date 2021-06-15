06077c804dee8e61ec810c039125dd788b61de32
qixiaoling <x.qi@novi-education.nl>
2021-06-15 18:32:49 +0200

    third commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useContext} from "react";
+import {fruitTeller} from "../App";
 
-export default function Home(props) {
-  const { totalFruit } = props;
-  return <div>{totalFruit} stuks fruit besteld </div>;
+export default function Home() {
+  const { apples, bananas, pears } = useContext(fruitTeller);
+  return <div>{apples + bananas + pears} stuks fruit besteld </div>;
 }
